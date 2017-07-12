8f2039a2cf30a5566e19b72fbf0e430a722496a7
Mitul Patel <mitul.dmg@gmail.com>
2017-07-11 22:21:04 -0400

    Added Async Method

--- AsyncAwaitDemo/AsyncAwaitDemo/Program.cs
+++ AsyncAwaitDemo/AsyncAwaitDemo/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AsyncAwaitDemo
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Calling AsyncAccessTheWeb method");
+            
+            Console.WriteLine("Print statement after async call from main");
+            Console.ReadKey();
+        }
+
+        async Task<int> AccessTheWebAsync()
+        {
+
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync("http://www.mitul.info");
+
+            DoIndependentTask();
+
+            string urlcontents = await getStringTask;
+
+            return urlcontents.Length;
+        }
+
+        private void DoIndependentTask()
+        {
+            Console.WriteLine("Printed before http call because of async call");
         }
     }
 }
